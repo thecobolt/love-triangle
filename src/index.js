2f54b7a0df159bdb2fe0842f17b1b667f1bcb919
thecobolt <thecobolt@gmail.com>
2018-09-24 17:00:43 +0300

    edit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
-/**
- * @param preferences - an array of integers. Indices of people, whom they love
- * @returns number of love triangles
- */
+
 module.exports = function getLoveTrianglesCount(preferences = []) {
   let sum = 0;
   for (let i = 0; i < preferences.length; i++) {
